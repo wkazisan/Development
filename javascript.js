5781daf12beac82c7efcffc73b9284fe3c778939
wkazisan <wkazisan@gmail.com>
2025-01-16 00:58:59 +0600

    js operator

--- javascript.js
+++
@@ -1,8 +0,0 @@
-// Date: 2021-09-06 - js where to
-        function fdemo() {
-            document.getElementById('demo').innerHTML = 'Show Time Over!';
-        }
-    function myf(){
-        document.getElementById('d2').innerHTML = 'Hlw Zisan!' ;
-        document.getElementById('d3').innerHTML = 'How are you!' ;
-    }
